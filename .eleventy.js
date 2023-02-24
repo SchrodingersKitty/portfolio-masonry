f26ab93da6dd9c466e250202e644a24060be410c
SchrodingersKitty <radium213@gmail.com>
2023-02-24 14:23:26 +0200

    Added fontawesome. Added socials data file.

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,10 @@ module.exports = config => {
   config.addPassthroughCopy({ './node_modules/photoswipe/dist/photoswipe-lightbox.esm.min.js': 'js/photoswipe-lightbox.esm.min.js' });
   config.addPassthroughCopy({ './node_modules/photoswipe/dist/photoswipe.esm.min.js': 'js/photoswipe.esm.min.js' });
   config.addPassthroughCopy({ './node_modules/photoswipe/dist/photoswipe.css': 'css/photoswipe.css' });
+  config.addPassthroughCopy({ './node_modules/@fortawesome/fontawesome-free/webfonts': 'webfonts' });
+  config.addPassthroughCopy({ './node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css': 'css/fontawesome.min.css' });
+  config.addPassthroughCopy({ './node_modules/@fortawesome/fontawesome-free/css/brands.min.css': 'css/brands.min.css' });
+  config.addPassthroughCopy({ './node_modules/@fortawesome/fontawesome-free/css/solid.min.css': 'css/solid.min.css' });
   config.addFilter('findURL', (url, collection) => {
     return collection.find(i => i.url == url);
   });
