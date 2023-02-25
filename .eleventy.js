c8f830b27a61d1bbcd5c89a35d137e8d8191ef05
SchrodingersKitty <radium213@gmail.com>
2023-02-25 19:14:07 +0200

    Redone and completely refactored styles.

--- .eleventy.js
+++ .eleventy.js
@@ -4,14 +4,16 @@ const sass = require("eleventy-sass");
 module.exports = config => {
   config.addPlugin(EleventyHtmlBasePlugin);
   config.addPlugin(sass);
-  config.addPassthroughCopy('./src/fonts');
-  config.addPassthroughCopy({ './node_modules/photoswipe/dist/photoswipe-lightbox.esm.min.js': 'js/photoswipe-lightbox.esm.min.js' });
-  config.addPassthroughCopy({ './node_modules/photoswipe/dist/photoswipe.esm.min.js': 'js/photoswipe.esm.min.js' });
-  config.addPassthroughCopy({ './node_modules/photoswipe/dist/photoswipe.css': 'css/photoswipe.css' });
-  config.addPassthroughCopy({ './node_modules/@fortawesome/fontawesome-free/webfonts': 'webfonts' });
-  config.addPassthroughCopy({ './node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css': 'css/fontawesome.min.css' });
-  config.addPassthroughCopy({ './node_modules/@fortawesome/fontawesome-free/css/brands.min.css': 'css/brands.min.css' });
-  config.addPassthroughCopy({ './node_modules/@fortawesome/fontawesome-free/css/solid.min.css': 'css/solid.min.css' });
+  config.addPassthroughCopy({
+    './src/fonts': 'fonts/',
+    './node_modules/photoswipe/dist/photoswipe-lightbox.esm.min.js': 'js/photoswipe-lightbox.esm.min.js',
+    './node_modules/photoswipe/dist/photoswipe.esm.min.js': 'js/photoswipe.esm.min.js',
+    './node_modules/photoswipe/dist/photoswipe.css': 'css/photoswipe.css',
+    './node_modules/@fortawesome/fontawesome-free/webfonts': 'webfonts',
+    './node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css': 'css/fontawesome.min.css',
+    './node_modules/@fortawesome/fontawesome-free/css/brands.min.css': 'css/brands.min.css',
+    './node_modules/@fortawesome/fontawesome-free/css/solid.min.css': 'css/solid.min.css'
+  });
   config.addFilter('findURL', (url, collection) => {
     return collection.find(i => i.url == url);
   });
